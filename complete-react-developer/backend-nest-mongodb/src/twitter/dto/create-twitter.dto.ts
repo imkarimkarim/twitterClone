0c1a7a08dac0f6f3f5734af19e0d678076d61b28
imkarimkarim <allalirezamail@gmail.com>
2022-07-03 16:52:01 +0430

    backend-nest-mongodb

---
+++ complete-react-developer/backend-nest-mongodb/src/twitter/dto/create-twitter.dto.ts
@@ -0,0 +1,11 @@
+export class CreateTwitterDto {
+  id?: string;
+  tweet: string;
+  img: string;
+}
+
+export class TwitterDto {
+  id?: string;
+  tweet: string;
+  img: string;
+}
---
+++ complete-react-developer/backend-nest-mongodb/src/twitter/twitter.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Twitter, TwitterSchema } from './schemas/twitter.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Twitter.name, schema: TwitterSchema }]),
+  ],
+  exports: [MongooseModule],
+})
+export class TwitterModule {}
